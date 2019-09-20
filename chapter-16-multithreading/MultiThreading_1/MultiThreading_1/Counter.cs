cbdd49a306cf4445a56cc6f56fcc7fb56db32614
Pavel Patrusov <pavelp9415@gmail.com>
2019-09-20 18:03:42 +0300

    Added section "Threads with parameters. ParameterizedThreadStart"

---
+++ chapter-16-multithreading/MultiThreading_1/MultiThreading_1/Counter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace MultiThreading_1
+{
+    public class Counter
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+
+        public Counter(int _x, int _y)
+        {
+            x = _x;
+            y = _y;
+        }
+
+        public void Count()
+        {
+            for (int i = 1; i < 9; i++)
+            {
+                Console.WriteLine("Second thread:");
+                Console.WriteLine(i * x * y);
+                Thread.Sleep(400);
+            }
+        }
+    }
+}
---
+++ chapter-16-multithreading/MultiThreading_1/MultiThreading_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+
+namespace MultiThreading_1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Counter counter_0 = new Counter(5, 4);
+
+            Thread MyThread_0 = new Thread(new ThreadStart(counter_0.Count));
+            MyThread_0.Start();
+        }
+    }
+}
