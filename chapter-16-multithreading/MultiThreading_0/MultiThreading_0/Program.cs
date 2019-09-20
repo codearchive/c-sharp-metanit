310a531a2fd57bf8231f7cefb9731cf1fdbef689
Pavel Patrusov <pavelp9415@gmail.com>
2019-09-20 17:28:14 +0300

    Added section "Creating threads. ThreadStart delegate"

---
+++ chapter-16-multithreading/MultiThreading_0/MultiThreading_0/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace MultiThreading_0
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Create new Thread
+            Thread myThread = new Thread(Count);
+            myThread.Start(); // Run Thread
+
+            for (int i = 1; i < 9; i++)
+            {
+                Console.WriteLine("Main Thread:");
+                Console.WriteLine(i * i);
+                Thread.Sleep(300);
+            }
+
+            Console.ReadLine();
+        }
+
+        public static void Count()
+        {
+            for (int i = 1; i < 9; i++)
+            {
+                Console.WriteLine("Second Thread:");
+                Console.WriteLine(i * i);
+                Thread.Sleep(400);
+            }
+        }
+    }
+}
